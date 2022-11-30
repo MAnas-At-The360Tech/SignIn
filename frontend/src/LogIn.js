6a748be4e5a20eac1c41c5284e81940c168acd61
MAnasThe360Tech <m.anaslatif.com@gmail.com>
2022-12-01 04:21:56 +0500

    add login

--- frontend/src/LogIn.js
+++ frontend/src/LogIn.js
@@ -1,10 +1,16 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import Context from './Context';
 
 export default function LogIn() {
+    const { setuser } = useContext(Context)
+    const navigate = useNavigate();
     const [LogInInput, setLogInInput] = useState({
         email: '',
         password: ''
     })
+
+    const [ResponsStatus, setResponsStatus] = useState('')
 
     const handleChange = (e) => {
         setLogInInput({
@@ -13,9 +19,34 @@ export default function LogIn() {
         });
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(LogInInput)
+
+        let headersList = {
+            "Content-Type": "application/json"
+        }
+
+        let bodyContent = JSON.stringify(LogInInput);
+
+        let url = 'http://127.0.0.1:3001/Login'
+        let response = await fetch(url, {
+            method: "POST",
+            body: bodyContent,
+            headers: headersList
+        });
+
+        let data = await response.text();
+        console.log(data);
+
+        if (response.status === 200) {
+            navigate("/")
+            let data = await response.json();
+            setuser(data)
+        } else {
+            setResponsStatus(response.status)
+        }
+
     }
 
     return (
@@ -36,6 +67,9 @@ export default function LogIn() {
                             <input name='password' type="password" onChange={handleChange} value={LogInInput.password} className="form-control" id="inputPassword" />
                         </div>
                     </div>
+
+                    {ResponsStatus === 404 ? <div className="alert alert-danger" role="alert">Place Enter valid Email</div> : ''}
+                    {ResponsStatus === 401 ? <div className="alert alert-danger" role="alert">Place Enter valid Password</div> : ''}
 
                     <div className="d-grid gap-2 d-md-flex justify-content-md-end">
                         <input type="submit" value="LogIn" className="btn btn-primary me-md-2" />
