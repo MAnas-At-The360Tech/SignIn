a44e2a89a56c89b2daa1154a38d0fee1cc4e0c5c
MAnasThe360Tech <m.anaslatif.com@gmail.com>
2022-12-01 02:56:18 +0500

    display respons

--- frontend/src/Confirm.js
+++ frontend/src/Confirm.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import Context from './Context';
 
 export default function Confirm() {
+    const { user } = useContext(Context)
     const [LogInInput, setLogInInput] = useState({
+        email: user.email,
         code: ''
     })
+    const [ResponseStatus, setResponseStatus] = useState('')
 
     const handleChange = (e) => {
         setLogInInput({
@@ -12,9 +16,27 @@ export default function Confirm() {
         });
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(LogInInput)
+
+
+        let headersList = {
+            "Content-Type": "application/json"
+        }
+
+        let bodyContent = JSON.stringify(LogInInput);
+
+        let url = "http://127.0.0.1:3001/confirm"
+        let response = await fetch(url, {
+            method: "POST",
+            body: bodyContent,
+            headers: headersList
+        });
+
+        setResponseStatus(response.status)
+        let data = await response.text();
+        console.log(data);
     }
 
     return (
@@ -26,12 +48,16 @@ export default function Confirm() {
                         <label htmlFor="code" className="col-sm-2 col-form-label">Code</label>
                         <div className="col-sm-10">
                             <input min='1000' max='9999' name='code' type="number" onChange={handleChange} value={LogInInput.code} className="form-control" id="code" />
+                            {ResponseStatus === 200 ? <div className="alert mt-3 alert-success" role="alert">Your Email is verify</div> : ''}
+                            {ResponseStatus === 406 ? <div className="alert mt-3 alert-danger" role="alert">You Enter Worng Pin</div> : ''}
+                            {ResponseStatus === 404 ? <div className="alert mt-3 alert-danger" role="alert">Your Email is Not Find</div> : ''}
                         </div>
                     </div>
                     <div className="d-grid gap-2 d-md-flex justify-content-md-end">
                         <input type="submit" value="Ok" className="btn btn-primary me-md-2" />
                     </div>
                 </form>
+
             </div>
         </div>
     )
